83f0d734a0264ade394fa73d7d74a3ad2dcaea64
Madisen <madisenvogel@gmail.com>
2022-10-30 18:21:14 -0600

    updates to server.js and added routes folder

--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,15 @@
 const express = require('express');
 const path = require('path');
-
+const notes = require('./db/db.json');
 const app = express();
 const PORT = process.env.port || 3001;
-const dir = path.join(__dirname, "/public");
-
-
-const notes = require('./db/db.json');
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
+// GET routes for html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
@@ -26,12 +18,11 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// GET route for API
 app.get('/api/notes', (req, res) => {
     res.json(notes.slice(1));
 });
 
-
-
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
